a3dcd9ef4201b2e04a2c057c441f46d01f8da1b2
KuringMIN <361118339@qq.com>
2020-12-14 15:58:30 +0000

    add auto deployment environment

---
+++ auto-deployment-env/main.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+cd ~/
+mkdir ~/.config
+mkdir ~/Github ~/prog ~/Pictures ~/Videos ~/Desktop ~/Documents ~/Downloads ~/Music ~/.zim ~/.config
+git clone https://github.com/KuringMIN/scripts ~/scripts
+git clone https://github.com/KuringMIN/.config ~/config
+cp -r ~/config/* ~/.config
+git clone https://github.com/KuringMIN/st ~/prog/st
+cd ~/prog/st
+sudo make clean install
+cd ~/
+git clone https://github.com/KuringMIN/dwm ~/prog/dwm
+cd ~/prog/dwm
+sudo make clean install
+cd ~/
+git clone https://github.com/KuringMIN/wallpapers ~/Pictures
+cd ~/
+
+sudo pamcan -S dmenu ranger fzf feh picom xorg-apps xorg-xinit xorg-server
+
+yay -S ttf-linux-libertine ttf-inconsolata ttf-joypixels ttf-twemoji-color noto-fonts-emoji ttf-liberation ttf-droid wqy-bitmapfont wqy-microhei wqy-microhei-lite wqy-zenhei adobe-source-han-mono-cn-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts otf-nerd-fonts-fira-code nerd-fonts-source-code-pro
+
+sudo chsh -s /usr/bin/zsh
+ln ~/.config/zsh/zshenv ~/.zshenv
+ln ~/.config/zsh/zshrc ~/.zshrc
+ln ~/.config/zsh/zlogin ~/.zlogin
+ln ~/.config/zsh/zimrc ~/.zimrc
+cp ~/.config/zsh/zimfw.zsh ~/.zim
+zsh ~/.zim/zimfw.zsh install
+zimfw install
+zimfw update
+zimfw upgrade
