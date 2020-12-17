e4a72b5d047bc101401ed1575177d161fed0dd6c
KuringMIN <361118339@qq.com>
2020-12-17 20:02:47 +0800

    update

--- auto-deployment-env/deploy-wm-sh.sh
+++ auto-deployment-env/deploy-wm-sh.sh
@@ -2,7 +2,7 @@
 
 cd ~/
 mkdir ~/Github ~/prog ~/Pictures ~/Videos ~/Desktop ~/Documents ~/Downloads ~/Music ~/.zim ~/.config
-git clone https://github.com/KuringMIN/scripts ~/scripts
+
 git clone https://github.com/KuringMIN/.config ~/config
 cp -r ~/config/* ~/.config
 git clone https://github.com/KuringMIN/st ~/prog/st
@@ -16,7 +16,14 @@ cd ~/
 git clone https://github.com/KuringMIN/wallpapers ~/Pictures
 cd ~/
 
-sudo pamcan -S dmenu neofetch chromium ranger fzf feh picom xorg-apps xorg-xinit xorg-server
+sudo pamcan -S dmenu neofetch chromium ranger fzf feh picom xorg-apps xorg-xinit xorg-server zsh network-manager-applet xfce4-power-manager trayer
+
+git clone https://aur.archlinux.org/yay.git ~/Github
+cd /Github/yay
+sudo makepkg -si
+cd ~/
+
+yay --aururl "https://aur.tuna.tsinghua.edu.cn" --save
 
 yay -S ttf-linux-libertine ttf-inconsolata ttf-joypixels ttf-twemoji-color noto-fonts-emoji ttf-liberation ttf-droid wqy-bitmapfont wqy-microhei wqy-microhei-lite wqy-zenhei adobe-source-han-mono-cn-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts otf-nerd-fonts-fira-code nerd-fonts-source-code-pro
 
