99cbf43672538b89e9a3405f7d0cf2bfeb9b12bf
KuringMIN <361118339@qq.com>
2020-12-17 20:41:11 +0800

    deploy nvim

---
+++ auto-deployment-env/deploy-nvim.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+
+sudo pacman -S python python2 python-pip python2-pip nodejs npm yarn ruby the_silver_searcher figlet xclip ctags:
+
+pip install pynvim debugpy ueberzug
+pip2 install pynvim
+
+nmp install -g neovim
+yarn global add neovim
+gem install neovim
+
