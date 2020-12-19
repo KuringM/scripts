f43b24939861913e2a5b0eafcd29d97e2b912603
KuringMIN <361118339@qq.com>
2020-12-19 17:16:45 +0800

    update

--- auto-deployment-env/deploy-nvim.sh
+++ auto-deployment-env/deploy-nvim.sh
@@ -1,11 +1,15 @@
 #! /usr/bin/bash
 
-sudo pacman -S python python2 python-pip python2-pip nodejs npm yarn ruby the_silver_searcher figlet xclip ctags:
+git clone https://github.com/KuingMIN/nvim ~/.config
+
+sudo pacman -S python python2 python-pip python2-pip nodejs npm yarn ruby the_silver_searcher figlet xclip ctags xdg-utils curl nodejs
 
 pip install pynvim debugpy ueberzug
 pip2 install pynvim
 
-nmp install -g neovim
+sudo nmp install -g neovim
 yarn global add neovim
 gem install neovim
 
+pip install --user smdv
+sudo nmp install -g instant-markdown-d
--- auto-deployment-env/deploy-wm-sh.sh
+++ auto-deployment-env/deploy-wm-sh.sh
@@ -16,7 +16,7 @@ cd ~/
 git clone https://github.com/KuringMIN/wallpapers ~/Pictures
 cd ~/
 
-sudo pamcan -S dmenu neofetch chromium ranger fzf feh picom xorg-apps xorg-xinit xorg-server zsh network-manager-applet xfce4-power-manager trayer
+sudo pamcan -S dmenu neofetch chromium ranger fzf feh picom xorg-apps xorg-xinit xorg-server zsh network-manager-applet xfce4-power-manager trayer flameshot alsa-utils screenkey
 
 git clone https://aur.archlinux.org/yay.git ~/Github
 cd /Github/yay
@@ -25,7 +25,7 @@ cd ~/
 
 yay --aururl "https://aur.tuna.tsinghua.edu.cn" --save
 
-yay -S ttf-linux-libertine ttf-inconsolata ttf-joypixels ttf-twemoji-color noto-fonts-emoji ttf-liberation ttf-droid wqy-bitmapfont wqy-microhei wqy-microhei-lite wqy-zenhei adobe-source-han-mono-cn-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts otf-nerd-fonts-fira-code nerd-fonts-source-code-pro
+yay -S ttf-linux-libertine ttf-inconsolata ttf-joypixels ttf-twemoji-color noto-fonts-emoji ttf-liberation ttf-droid wqy-bitmapfont wqy-microhei wqy-microhei-lite wqy-zenhei adobe-source-han-mono-cn-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts otf-nerd-fonts-fira-code nerd-fonts-source-code-pro acpitool
 
 sudo chsh -s /usr/bin/zsh
 ln ~/.config/zsh/zshenv ~/.zshenv
